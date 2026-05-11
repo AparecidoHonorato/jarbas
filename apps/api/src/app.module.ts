35764fc118d7fd2e24ab8faf12503516450f224b
AparecidoHonorato <aparecidogomes1003@gmail.com>
2026-05-11 18:17:37 -0300

    feat: registrar JobsModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { ChatModule } from './modules/chat/chat.module';
 import { ProvidersModule } from './modules/providers/providers.module';
 import { RoutingModule } from './modules/routing/routing.module';
+import { JobsModule } from './modules/jobs/jobs.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { RoutingModule } from './modules/routing/routing.module';
     ChatModule,
     ProvidersModule,
     RoutingModule,
+    JobsModule,
   ],
 })
 export class AppModule {}
