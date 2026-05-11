7c7b3c205c752cf1eebfcb672aa997f7d5f4db4d
AparecidoHonorato <aparecidogomes1003@gmail.com>
2026-05-11 18:14:45 -0300

    feat: jobs module no backend

--- apps/api/src/modules/jobs/jobs.module.ts
+++ apps/api/src/modules/jobs/jobs.module.ts
@@ -1,5 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ChatModule } from './modules/chat/chat.module';
+import { ProvidersModule } from './modules/providers/providers.module';
+import { RoutingModule } from './modules/routing/routing.module';
 import { JobsModule } from './modules/jobs/jobs.module';
 
 @Module({
-  imports: [..., JobsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ChatModule,
+    ProvidersModule,
+    RoutingModule,
+    JobsModule,
+  ],
 })
+export class AppModule {}
